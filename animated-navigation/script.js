34cd3e4567b266a40ea69b46e7e1297d2ba358a0
이혜천 <rainbow880616@gmail.com>
2021-08-31 22:55:59 +0900

    로그삭제.

--- animated-navigation/script.js
+++ animated-navigation/script.js
@@ -1,6 +1,5 @@
 const toggle = document.querySelector("#toggle");
 const nav = document.querySelector("#nav");
 toggle.addEventListener('click', function (e) {
-  console.log("===")
   nav.classList.toggle('active');
 })
